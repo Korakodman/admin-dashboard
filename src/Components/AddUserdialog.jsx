21d75057adc518f6c2e3bd243c91a21376c05230
Enola8847 <ice9393th@gmail.com>
2025-02-23 21:26:58 +0700

    dialog add user 100%

--- src/Components/AddUserdialog.jsx
+++ src/Components/AddUserdialog.jsx
@@ -1,61 +1,101 @@
 import React from "react";
 import { forwardRef } from "react";
 import style from "./AddUserdialog.module.css";
-const AddUserdialog = forwardRef(({ clickoutside, Closedialog }, ref) => {
-  return (
-    <div>
-      <dialog
-        ref={ref}
-        className="w-[400px] h-fit rounded-md p-4 border-black bg-gray-200"
-        onClick={clickoutside} // ใช้ฟังก์ชันที่แก้ไขแล้ว
-      >
-        <form className="p-4 rounded-md">
-          <div className="grid p-2 mb-2">
-            <div className="text-2xl mb-2 text-center">
-              <h1>Add User</h1>
-            </div>
-            <input placeholder="FirstName" className={style.UserInput} />
-            <input placeholder="LastName" className={style.UserInput} />
-            <input placeholder="Password" className={style.UserInput} />
-            <div className="flex ">
+const AddUserdialog = forwardRef(
+  (
+    {
+      clickoutside,
+      Closedialog,
+      AddNewUser,
+      msgeEror,
+      error,
+      handleInputFirstName,
+      handleInputLastname,
+      handleInputPassword,
+      handleInputrole,
+      radioCheck,
+      AddUser,
+    },
+    ref
+  ) => {
+    return (
+      <div>
+        <dialog
+          ref={ref}
+          className="w-[400px] h-fit rounded-md p-4 border-black bg-gray-200 shadow-md"
+          onClick={clickoutside} // ใช้ฟังก์ชันที่แก้ไขแล้ว
+        >
+          <form className="p-4 rounded-md" onSubmit={(e) => AddUser(e)}>
+            <div className="grid p-2 mb-2">
+              <div className="text-2xl mb-2 text-center">
+                <h1>Add User</h1>
+              </div>
               <input
-                type="radio"
-                id="Admin"
-                name="role"
-                value="Admin"
-                className="mr-2"
+                placeholder="FirstName"
+                onChange={(e) => handleInputFirstName(e)}
+                value={AddNewUser.UserName}
+                className={style.UserInput}
               />
-              <label htmlFor="Admin" className="mr-2">
-                Admin
-              </label>
               <input
-                type="radio"
-                id="User"
-                name="role"
-                value="User"
-                className="mr-2"
+                placeholder="LastName"
+                onChange={(e) => handleInputLastname(e)}
+                value={AddNewUser.LastName}
+                className={style.UserInput}
               />
-              <label htmlFor="User">User</label>
+              <input
+                placeholder="Password"
+                type="password"
+                onChange={(e) => handleInputPassword(e)}
+                value={AddNewUser.PassWord}
+                className={style.UserInput}
+              />
+              <div className="flex ">
+                <input
+                  type="radio"
+                  id="Admin"
+                  value="Admin"
+                  name="role"
+                  className="mr-2"
+                  checked={radioCheck === "Admin"}
+                  onChange={handleInputrole}
+                />
+                <label htmlFor="Admin" className="mr-2">
+                  Admin
+                </label>
+                <input
+                  type="radio"
+                  id="User"
+                  value="User"
+                  name="role"
+                  className="mr-2"
+                  onChange={handleInputrole}
+                  checked={radioCheck === "User"}
+                />
+                <label htmlFor="User">User</label>
+              </div>
             </div>
-          </div>
-          <div>
-            <button
-              className="bg-blue-600 hover:bg-blue-700 px-3 py-2 rounded text-white ml-4"
-              type="button"
-            >
-              Add New User
-            </button>
-            <button
-              className="bg-slate-600 hover:bg-slate-700 px-3 py-2 rounded text-white ml-4"
-              type="button"
-              onClick={Closedialog}
-            >
-              Close
-            </button>
-          </div>
-        </form>
-      </dialog>
-    </div>
-  );
-});
+            <div>
+              <button
+                className="bg-blue-600 hover:bg-blue-700 px-3 py-2 rounded text-white ml-4"
+                type="submit"
+              >
+                Add New User
+              </button>
+              <button
+                className="bg-slate-600 hover:bg-slate-700 px-3 py-2 rounded text-white ml-4"
+                type="button"
+                onClick={Closedialog}
+              >
+                Close
+              </button>
+            </div>
+            <div className=" text-red-500 mt-2 ml-4 font-bold text-xl">
+              {error ? <span>{msgeEror}</span> : ""}
+            </div>
+          </form>
+        </dialog>
+      </div>
+    );
+  }
+);
 export default AddUserdialog;
--- src/app/users/page.js
+++ src/app/users/page.js
@@ -5,7 +5,9 @@ import Table from "../../Components/Table";
 import AddUserdialog from "@/Components/AddUserdialog";
 export default function Users() {
   const [error, Seterror] = useState(false);
+  const [msgeEror, SetmsgeError] = useState();
   const [Users, setUsers] = useState([]);
+  const [radioCheck, SetradioCheck] = useState("");
   const [AddNewUser, setAddNewUser] = useState({
     UserName: "",
     LastName: "",
@@ -19,19 +21,37 @@ export default function Users() {
     }
   }
   function Closedialog() {
+    setAddNewUser({ UserName: "", LastName: "", role: "", PassWord: "" });
     dialog.current.close();
   }
   const clickoutside = (e) => {
     if (e.target === dialog.current) {
+      setAddNewUser({ UserName: "", LastName: "", role: "", PassWord: "" });
       Closedialog();
     }
   };
   const AddUser = (e) => {
     e.preventDefault();
-    console.log(AddNewUser);
-    setUsers((prevNewUser) => [...prevNewUser, AddNewUser]);
-    setAddNewUser({ UserName: "", LastName: "", role: "", PassWord: "" });
-    Closedialog();
+
+    if (AddNewUser.UserName == "") {
+      SetmsgeError("ใส่ชื่อด้วยครับ");
+      Seterror(true);
+    } else if (AddNewUser.LastName == "") {
+      SetmsgeError("ใส่นามสกุลด้วยครับ");
+      Seterror(true);
+    } else if (AddNewUser.PassWord == "") {
+      SetmsgeError("ใส่รหัสด้วยครับ");
+      Seterror(true);
+    } else if (AddNewUser.role == "") {
+      SetmsgeError("ใส่ยศด้วยครับ");
+      Seterror(true);
+    } else {
+      setUsers((prevNewUser) => [...prevNewUser, AddNewUser]);
+      setAddNewUser({ UserName: "", LastName: "", role: "", PassWord: "" });
+      SetradioCheck("");
+      Seterror(false);
+      Closedialog();
+    }
   };
   const handleInputFirstName = (e) => {
     setAddNewUser({
@@ -56,6 +76,7 @@ export default function Users() {
       ...AddNewUser,
       role: e.target.value,
     });
+    SetradioCheck(e.target.value);
   };
   // console.log(Users);
   return (
@@ -69,78 +90,20 @@ export default function Users() {
           Add User
         </button>
       </div>
-      {/* <AddUserdialog
+      <AddUserdialog
         ref={dialog}
         Closedialog={Closedialog}
         clickoutside={clickoutside}
-      /> */}
-      <dialog
-        ref={dialog}
-        className="w-[400px] h-fit rounded-md p-4 border-black bg-gray-200"
-        onClick={clickoutside} // ใช้ฟังก์ชันที่แก้ไขแล้ว
-      >
-        <form className="p-4 rounded-md" onSubmit={(e) => AddUser(e)}>
-          <div className="grid p-2 mb-2">
-            <div className="text-2xl mb-2 text-center">
-              <h1>Add User</h1>
-            </div>
-            <input
-              placeholder="FirstName"
-              onChange={(e) => handleInputFirstName(e)}
-              value={AddNewUser.UserName}
-            />
-            <input
-              placeholder="LastName"
-              onChange={(e) => handleInputLastname(e)}
-              value={AddNewUser.LastName}
-            />
-            <input
-              placeholder="Password"
-              type="password"
-              onChange={(e) => handleInputPassword(e)}
-              value={AddNewUser.PassWord}
-            />
-            <div className="flex ">
-              <input
-                type="radio"
-                id="Admin"
-                value="Admin"
-                name="role"
-                className="mr-2"
-                onChange={handleInputrole}
-              />
-              <label htmlFor="Admin" className="mr-2">
-                Admin
-              </label>
-              <input
-                type="radio"
-                id="User"
-                value="User"
-                name="role"
-                className="mr-2"
-                onChange={handleInputrole}
-              />
-              <label htmlFor="User">User</label>
-            </div>
-          </div>
-          <div>
-            <button
-              className="bg-blue-600 hover:bg-blue-700 px-3 py-2 rounded text-white ml-4"
-              type="submit"
-            >
-              Add New User
-            </button>
-            <button
-              className="bg-slate-600 hover:bg-slate-700 px-3 py-2 rounded text-white ml-4"
-              type="button"
-              onClick={Closedialog}
-            >
-              Close
-            </button>
-          </div>
-          <div>{error ? "Please Insert all" : ""}</div>
-        </form>
-      </dialog>
+        AddUser={AddUser}
+        AddNewUser={AddNewUser}
+        msgeEror={msgeEror}
+        error={error}
+        handleInputFirstName={handleInputFirstName}
+        handleInputLastname={handleInputLastname}
+        handleInputPassword={handleInputPassword}
+        handleInputrole={handleInputrole}
+        radioCheck={radioCheck}
+      />
 
       <Table Users={Users} />
     </main>
