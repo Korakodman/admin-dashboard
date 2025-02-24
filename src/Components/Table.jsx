c3429fa4e128e6a3d29e3392d532eab7a5d19f68
Enola8847 <ice9393th@gmail.com>
2025-02-24 11:51:12 +0700

    Information and Delete btn

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 function Table({ Users }) {
+  const FormDialog = useRef();
+  const [DialogOption, SetDialogOption] = useState();
+  const [SelectUser, SetSelectUser] = useState([]);
+  const OpenDialog = (user) => {
+    FormDialog.current.showModal();
+    console.log(user);
+    SetSelectUser(user);
+  };
+  const CloseDialog = () => {
+    FormDialog.current.close();
+  };
+  const clickoutside = (e) => {
+    if (e.target === FormDialog.current) {
+      CloseDialog();
+    }
+  };
+  const FormOption = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="p-4 font-serif">
       <table className="min-w-full bg-gray-800 text-white border border-gray-600">
@@ -25,13 +44,19 @@ function Table({ Users }) {
               <td className="p-2 text-center">
                 <button
                   className="bg-blue-600 hover:bg-blue-700 px-3 py-1 rounded text-white"
-                  onClick={() => alert(`ดูรายละเอียดของ ${user.UserName}`)}
+                  onClick={() => {
+                    OpenDialog(user);
+                    SetDialogOption(false);
+                  }}
                 >
                   ดูรายละเอียด
                 </button>
                 <button
                   className="bg-red-600 hover:bg-red-700 px-3 py-1 ml-2 rounded text-white"
-                  onClick={() => alert(`ลบข้อมูลของ ${user.UserName}`)}
+                  onClick={() => {
+                    OpenDialog(user);
+                    SetDialogOption(true);
+                  }}
                 >
                   ลบ
                 </button>
@@ -40,6 +65,26 @@ function Table({ Users }) {
           ))}
         </tbody>
       </table>
+      <div>
+        <dialog
+          className="w-[400px] h-fit rounded-md p-4 border-black bg-gray-200 shadow-md "
+          onClick={(e) => clickoutside(e)}
+          ref={FormDialog}
+        >
+          <form onSubmit={(e) => FormOption(e)}>
+            <div>{DialogOption ? "Delete User" : "Edit User"}</div>
+            <div>Name{DialogOption ? "" : SelectUser.UserName}</div>
+            <div className=" flex justify-end">
+              <button
+                className=" bg-gray-400 p-2 hover:bg-gray-500 rounded-md"
+                onClick={CloseDialog}
+              >
+                Close
+              </button>
+            </div>
+          </form>
+        </dialog>
+      </div>
     </div>
   );
 }
