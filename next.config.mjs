0a9c0d67b47d331f5b8a7f1de2816063594d8f1e
Enola8847 <ice9393th@gmail.com>
2025-03-14 11:11:53 +0700

    fix it

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  output: "export", // ✅ ใช้ Next.js Static Export
+  basePath: isProd ? "/admin-dashboard" : "", // ✅ เปลี่ยน <repo-name> เป็นชื่อ repo ของคุณ
+  assetPrefix: isProd ? "/admin-dashboard" : "",
+  trailingSlash: true, // ✅ ช่วยให้เส้นทางไฟล์ทำงานถูกต้อง
+};
 
 export default nextConfig;
