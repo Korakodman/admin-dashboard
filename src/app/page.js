2d6a6c9e0b7720d39e213da08f3bc886fc2a950c
Enola8847 <ice9393th@gmail.com>
2025-03-23 16:47:49 +0700

    just fix something not big problem

--- src/app/page.js
+++ src/app/page.js
@@ -16,48 +16,81 @@ export default function Home() {
   return (
     <main className="bg-gray-100 md:h-screen md:w-screen">
       <section className="grid justify-center bg-gradient-to-br from-fuchsia-600 to-cyan-500 md:h-screen ">
-        <div className="grid justify-center items-center h-auto">
-          <h1 className="md:text-3xl font-bold text-white p-2">
-            {Isregister ? "Login Page" : "Register Page"}
-          </h1>
-        </div>
-        <div className="mb-40 ">
-          <form className="border border-white p-6 rounded-md bg-white ">
-            <div className="grid">
-              <div className="text-black font-bold text-1xl mb-4">
-                <label className="">Username*</label>
-                <input
-                  placeholder="Username"
-                  className={style.inputuser}
-                  type="text"
-                />
-              </div>
-              <div className="text-black font-bold text-1xl mb-4">
-                <label>Password*</label>
-                <input
-                  placeholder="Password"
-                  className={style.inputuser}
-                  type="password"
-                />
-              </div>
+        <div className="mt-36">
+          <form className="border border-white p-6 rounded-md bg-white w-[400px] ">
+            <div className="grid justify-center items-center h-auto">
+              <h1 className="md:text-3xl font-bold text-black p-2">
+                {Isregister ? "Login Page" : "Register Page"}
+              </h1>
             </div>
+            {Isregister ? (
+              <div className="grid">
+                <div className="text-black font-bold text-1xl mb-4">
+                  <input
+                    placeholder="Username"
+                    className={style.inputuser}
+                    type="text"
+                  />
+                </div>
+                <div className="text-black font-bold text-1xl mb-4">
+                  <input
+                    placeholder="Password"
+                    className={style.inputuser}
+                    type="password"
+                  />
+                </div>
+              </div>
+            ) : (
+              <div className="grid">
+                <div className="text-black font-bold text-1xl mb-4">
+                  <input
+                    placeholder="Username"
+                    className={style.inputuser}
+                    type="text"
+                  />
+                </div>
+                <div className="text-black font-bold text-1xl mb-4">
+                  <input
+                    placeholder="Lastname"
+                    className={style.inputuser}
+                    type="text"
+                  />
+                </div>
+                <div className="text-black font-bold text-1xl mb-4">
+                  <input
+                    placeholder="Password"
+                    className={style.inputuser}
+                    type="password"
+                  />
+                </div>
+                <div className="text-black font-bold text-1xl mb-4">
+                  <input
+                    placeholder="Repeat Password"
+                    className={style.inputuser}
+                    type="password"
+                  />
+                </div>
+              </div>
+            )}
             <div className=" flex justify-end ">
               <button
                 type="button"
                 onClick={() => SetIslogin(true)}
                 className="w-full  text-white font-bold py-2 px-4 bg-gradient-to-br from-fuchsia-600 to-cyan-500 p-2  mt-4"
               >
-                Login
+                {Isregister ? "Login" : "Register"}
               </button>
             </div>
-            <div className=" text-black mt-4 font-bold md:text-lg">
-              Don't Have an Account?
+            <div className=" text-black mt-4 font-bold md:text-lg text-center">
+              {Isregister
+                ? " Don't Have an Account? "
+                : "Already Have an Account? "}
               <button
                 type="button"
                 className="text-blue-600"
                 onClick={ReisterPage}
               >
-                Sign in
+                {Isregister ? "Sign Up" : "Login"}
               </button>
             </div>
           </form>
